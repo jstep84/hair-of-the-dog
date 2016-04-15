d13f88d07a05a29922bb6acba27880fc54843f52
John Stepan <jstep84@gmail.com>
2016-04-15 02:59:31 +0000

    map test

--- site.js
+++ site.js
@@ -4,3 +4,11 @@ $(".dog").click(function() {
       var swap = _this.attr("data-swap");     
      _this.attr('src', swap).attr("data-swap",current);   
 }); 
+
+function initMap() {
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+          center: {lat: 44.540, lng: -78.546},
+          zoom: 8
+        });
+      }
